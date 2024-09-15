49f30bd27485d189b894d0d16f5cfae945bf9f4c
danmlarsen <larsendanmarius@gmail.com>
2024-09-15 13:53:40 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts}'],
+    theme: {
+        extend: {
+            colors: {
+                grey: {
+                    500: 'hsl(0, 0%, 55%)',
+                    600: 'hsl(0, 0%, 41%)',
+                    default: 'hsl(0, 0%, 41%)',
+                },
+            },
+            fontFamily: {
+                sans: ['Alata', 'Josefin Sans', 'sans-serif'],
+                heading: ['Josefin Sans', 'Alata', 'sans-serif'],
+            },
+            backgroundImage: {
+                hero: 'url("/images/mobile/image-hero.jpg")',
+                'hero-desktop': 'url("/images/desktop/image-hero.jpg")',
+                interactive: 'url("/images/mobile/image-interactive.jpg")',
+                interactive: 'url("/images/desktop/image-interactive.jpg")',
+            },
+        },
+    },
+    plugins: [],
+};
